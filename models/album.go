ce9d7e20f977737ac337f65c642d81bd6a214748
Om Ra <me@xomrkob.ru>
2025-09-24 13:55:14 +0600

    add Get for Artist

--- models/album.go
+++ models/album.go
@@ -3,6 +3,7 @@ package models
 import "time"
 
 type Album struct {
+	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
 	Artist    string    `json:"artist"`
 	Price     float64   `json:"price"`
